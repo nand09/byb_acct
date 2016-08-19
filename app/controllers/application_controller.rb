90a085b9101f4480d20d25d3e656a33c22d8afa3
Fernando <fosorto99@gmail.com>
2016-08-19 16:29:55 +0000

    Added home, about, contact, pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def Home
+    render text: 'This will be the home page'
+  end
+
 end
